5a58837d5ee80e2c6ddf2cd1f978b302d0c4385b
harim <harim9504@gmail.com>
2023-09-29 16:19:49 +0900

    first commit

---
+++ src/test/java/com/example/justmeet/JustMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.justmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustMeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
